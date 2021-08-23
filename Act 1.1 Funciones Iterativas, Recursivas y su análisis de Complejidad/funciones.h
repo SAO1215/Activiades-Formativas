94af7867cd388949777091fa390ff7f65635675e
SAO1215 <70983072+SAO1215@users.noreply.github.com>
2021-08-23 12:19:58 -0500

    Add files via upload

---
+++ Act 1.1 Funciones Iterativas, Recursivas y su análisis de Complejidad/funciones.h
@@ -0,0 +1,44 @@
+/* 
+ * 
+ * Programación de estructuras de datos y algoritmos fundamentales (TC1031.2)
+ * Formativa 1.1 Funciones Iterativas, Recursivas y su análisis de Complejidad
+ * Olivia Araceli Morales Quezada
+ * A01707371
+ *
+*/
+
+#ifndef FUNCIONES_H_
+#define FUNCIONES_H_
+
+#include <iostream>
+
+class Funciones
+{
+public:
+    int sumaIterativa(int n);
+    int sumaRecursiva(int n);
+    int sumaDirecta(int n);
+};
+
+int Funciones::sumaIterativa(int n){
+	int sum = 0;
+	for (int i = 1; i <= n; i++) {
+		sum += i;
+	}
+	return sum;
+}
+
+int Funciones::sumaRecursiva(int n){
+	if (n == 0) {
+		return 0;
+	} else {
+		return n + sumaRecursiva(n - 1);
+	}
+}
+
+int Funciones::sumaDirecta(int n){
+  int sum = (n*(n+1))/2;
+  return sum;
+}
+
+#endif /* FUNCIONES_H_ */
